38e9220d53e7d35c3b2162adb6fb8fe1cc19b4ea
JasperEbus <j.j.h.ebus@gmail.com>
2021-03-24 17:18:47 +0100

    Small changes

--- Roboflow_sampler.py
+++ Roboflow_sampler.py
@@ -11,7 +11,7 @@ file_list = glob.glob('*.jpg')
 # write each path to image in the vm folder in a .txt file
 with open('garb_test.txt', 'a') as the_file:
     for i in file_list:
-        path_name= './data_12/images/'+i
+        path_name= './data_12classes/images/'+i
         print(path_name)
         the_file.write((path_name+ '\n'))
 
